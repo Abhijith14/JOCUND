652e9b95c4122bb436e77a08c55fea06eeb534fa
Shaino Sajimon <shaino94@gmail.com>
2022-07-09 17:29:48 +0530

    first commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+$(".toggle-password").click(function() {
+    $(this).toggleClass("fa-eye fa-eye-slash");
+
+    var input = $($(this).attr("toggle"));
+
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    } else {
+        input.attr("type", "password");
+    }
+});
